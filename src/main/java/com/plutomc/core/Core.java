9b6cb00ca5cf7b1ceafbd098f50d7db28fb5337f
Kevin Boxhoorn <kevinboxhoorn@yahoo.com>
2016-11-17 20:17:39 +1100

    Initial changes

--- src/main/java/com/plutomc/core/Core.java
+++ src/main/java/com/plutomc/core/Core.java
@@ -1,20 +1,19 @@
-package com.example.examplemod;
+package com.plutomc.core;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = Core.MODID, version = Core.VERSION)
+public class Core
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "plutomc-core";
     public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+
     }
 }
