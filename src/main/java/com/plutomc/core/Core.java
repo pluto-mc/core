69c9b87708bdf081f49e7cc9648eb201073f126f
Kevin Boxhoorn <kevinboxhoorn@yahoo.com>
2016-11-18 06:56:12 +1100

    Update code style

--- src/main/java/com/plutomc/core/Core.java
+++ src/main/java/com/plutomc/core/Core.java
@@ -1,6 +1,5 @@
 package com.plutomc.core;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -8,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = Core.MODID, version = Core.VERSION)
 public class Core
 {
-    public static final String MODID = "plutomc-core";
-    public static final String VERSION = "1.0";
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+	public static final String MODID = "plutomc-core";
+	public static final String VERSION = "1.0";
 
-    }
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+
+	}
 }
