be6a75f8b8f7feaa7d93277b406fd6625bdf2507
Kevin Boxhoorn <kevinboxhoorn@yahoo.com>
2016-11-19 17:32:54 +1100

    Update version

--- src/main/java/com/plutomc/core/Core.java
+++ src/main/java/com/plutomc/core/Core.java
@@ -33,7 +33,7 @@ public class Core
 {
 	public static final String MOD_ID = "plutomc_core";
 	public static final String NAME = "PlutoMC Core";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "0.1.0";
 
 	public static final String CLIENT_PROXY_CLASS = "com.plutomc.core.client.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.plutomc.core.server.ServerProxy";
