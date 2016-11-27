9fa9cfdb5d15a569454c7a44075c127b2d6c1c8b
Kevin Boxhoorn <kevinboxhoorn@yahoo.com>
2016-11-27 20:57:31 +1100

    Continue Alloy Furnace
    TODO: Fix missing textures/variants.

--- src/main/java/com/plutomc/core/common/blocks/BlockAlloyFurnace.java
+++ src/main/java/com/plutomc/core/common/blocks/BlockAlloyFurnace.java
@@ -2,14 +2,15 @@ package com.plutomc.core.common.blocks;
 
 import com.plutomc.core.common.tileentities.TileEntityAlloyFurnace;
 import com.plutomc.core.init.BlockRegistry;
-import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -35,13 +36,13 @@ import java.util.Random;
  */
 public class BlockAlloyFurnace extends BaseBlockContainer
 {
-	public static final PropertyDirection FACING = BlockDirectional.FACING;
+	public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
 	public static final PropertyBool BURNING = PropertyBool.create("burning");
 
 	public BlockAlloyFurnace()
 	{
 		super(BlockRegistry.Data.ALLOY_FURNACE);
-		setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(BURNING, Boolean.FALSE));
+		setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(BURNING, false));
 		setHardness(3.5f);
 		setSoundType(SoundType.STONE);
 	}
@@ -55,54 +56,19 @@ public class BlockAlloyFurnace extends BaseBlockContainer
 	@Override
 	public IBlockState getStateFromMeta(int meta)
 	{
-		return this.getDefaultState().withProperty(FACING, EnumFacing.getFront(meta & 7)).withProperty(BURNING, Boolean.valueOf((meta & 8) > 0));
+		return getDefaultState().withProperty(FACING, EnumFacing.getFront(meta & 7)).withProperty(BURNING, (meta & 8) > 0);
 	}
 
 	@Override
 	public int getMetaFromState(IBlockState state)
 	{
 		int i = 0;
-		i = i | state.getValue(FACING).getIndex();
-		if (Boolean.valueOf(state.getValue(BURNING)))
+		i |= state.getValue(FACING).getIndex();
+		if (state.getValue(BURNING))
 		{
 			i |= 8;
 		}
 		return i;
-	}
-
-	private void setDefaultDirection(World worldIn, BlockPos pos, IBlockState state)
-	{
-		if (!worldIn.isRemote)
-		{
-			EnumFacing enumfacing = state.getValue(FACING);
-			boolean flagN = worldIn.getBlockState(pos.north()).isFullBlock();
-			boolean flagS = worldIn.getBlockState(pos.south()).isFullBlock();
-
-			if (enumfacing == EnumFacing.NORTH && flagN && !flagS)
-			{
-				enumfacing = EnumFacing.SOUTH;
-			}
-			else if (enumfacing == EnumFacing.SOUTH && flagS && !flagN)
-			{
-				enumfacing = EnumFacing.NORTH;
-			}
-			else
-			{
-				boolean flagW = worldIn.getBlockState(pos.west()).isFullBlock();
-				boolean flagE = worldIn.getBlockState(pos.east()).isFullBlock();
-
-				if (enumfacing == EnumFacing.WEST && flagW && !flagE)
-				{
-					enumfacing = EnumFacing.EAST;
-				}
-				else if (enumfacing == EnumFacing.EAST && flagE && !flagW)
-				{
-					enumfacing = EnumFacing.WEST;
-				}
-			}
-
-			worldIn.setBlockState(pos, state.withProperty(FACING, enumfacing).withProperty(BURNING, Boolean.FALSE), 2);
-		}
 	}
 
 	@Override
@@ -119,8 +85,20 @@ public class BlockAlloyFurnace extends BaseBlockContainer
 	}
 
 	@Override
+	public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+	{
+		return getDefaultState().withProperty(FACING, EnumFacing.fromAngle(placer.getRotationYawHead())).withProperty(BURNING, false);
+	}
+
+	@Override
 	public Item getItemDropped(IBlockState state, Random rand, int fortune)
 	{
 		return BlockRegistry.ALLOY_FURNACE;
+	}
+
+	@Override
+	public EnumBlockRenderType getRenderType(IBlockState state)
+	{
+		return EnumBlockRenderType.MODEL;
 	}
 }
